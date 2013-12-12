77f86a0bd6bcaf5f36accbcb673360826cac7db4
Chad Seaman <chadillac83@gmail.com>
2013-12-11 20:48:03 -0800

    - added failover support for older browsers (dump log calls to textarea) - enabled debugging by default

--- smarter_console.js
+++ smarter_console.js
@@ -1,12 +1,61 @@
 // smarter console
 (function(console_ref){
-    var debugging = false;
-    var safety = function() { /* do nothing, avoid exceptions. */ };
+    var debugging = true;
+    var old_support = false;
+    var failover_console = false;
+
+    // method we'll shove in place to catch common use cases, also supports
+    // using self built console if support is enabled and an existing console isn't
+    // available for us to use (IE I'm looking at you...)
+    var failover = function() {
+        if (debugging) {
+            if (failover_console) {
+                var log_time = new Date();
+                var append_it = [];
+                for (var arg in arguments) {
+                    append_it.push(' '+arguments[arg]);    
+                }
+                append_it.join(',');
+                failover_console.value += "["+log_time.toTimeString()+"]-----------\n";
+                failover_console.value += " >"+append_it+"\n";
+                failover_console.scrollTop = failover_console.scrollHeight;
+            }
+        }
+    };
+
+    // they want to utilize failover console for older browers, do it up.
+    var enable_failover = function(attempts) {
+        if (typeof attempts == 'undefined') {
+            attempts = 0;   
+        }
+        if (document.readyState != "interactive" && document.readyState != 'complete') {
+            // DOM isn't ready yet, retry later.
+            if (attempts <= 200) {
+                // we'll try for 200 attempts, then give up.
+                setTimeout(enable_failover,100,attempts);
+            }
+            return; // try again shortly
+        }
+        if (debugging) {
+            // create and style our failover console
+            var console_out = document.createElement('textarea'); 
+            console_out.style.position = "absolute";
+            console_out.style.bottom =  "0"; 
+            console_out.style.right = "0"; 
+            console_out.style.left = "0"; 
+            console_out.style.height = "200px";
+            console_out.readOnly = true;
+            document.body.appendChild(console_out);
+            failover_console = console_out;
+        }
+    };
+
     var console = {
         // add safety handler for most common use cases 
-        log:safety, 
-        warn:safety, 
-        trace:safety,
+        log:failover, 
+        warn:failover, 
+        trace:failover,
+        enable_failover_support: enable_failover,
         // enabled debugging in production enviornments via console.enable_debugging
         enable_debugging: function() {
             debugging = true;
@@ -25,7 +74,7 @@
     // mirror source, adding our can_use checking logic 
     if (console_ref) {
         for (var method in console_ref) {
-            if (typeof console[method] == 'undefined' || console[method] === safety) {
+            if (typeof console[method] == 'undefined' || console[method] === failover) {
                 // create it, closure is to ensure method name sticks
                 (function(method_name){
                      console[method_name] = function() {
@@ -36,6 +85,8 @@
                 })(method);
             }
         }
+    } else if (old_support) {
+        enable_failover();    
     }
 
     // enabled debugging by default on dev/staging 
